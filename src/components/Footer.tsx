053d9088449e2d820cf5f5627bfefc015e5641a9
Miguel Ângelo <miguelcg958@gmail.com>
2022-03-01 15:09:00 -0300

    chore: update some details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="flex justify-center w-full px-6 py-3 border-t border-gray-200 backdrop-blur bg-white/95">
       {/* Footer Wrapper */}
-      <div className="flex items-center w-full max-w-6xl">
+      <div className="flex items-center w-full max-w-6xl justify-center md:justify-start">
         <p className="text-sm font-light text-gray-700">
           &copy; {new Date().getFullYear()} - Todos os direitos reservados.
         </p>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,11 +7,11 @@ export const HeroSection = () => {
   return (
     <section className="px-6 py-8 pt-16 lg:py-16">
       {/* Content wrapper */}
-      <div className="flex flex-col items-center w-full max-w-5xl gap-16 mx-auto lg:flex-row">
+      <div className="flex flex-col items-center w-full max-w-5xl gap-8 lg:gap-16 mx-auto lg:flex-row">
         {/* Texts */}
         <div className="flex flex-col flex-1 max-w-lg lg:max-w-none">
           <h1 className="text-4xl font-bold text-center text-gray-900 lg:text-left lg:text-6xl">
-            Desenvolvedor Front-End Júnior focado em <span className="text-teal-500">ReactJS</span>
+            Desenvolvedor Front-End Júnior focado em <span className="text-teal-400">ReactJS</span>
           </h1>
           <p className="mt-2 text-base font-light text-center text-gray-600 lg:text-left">
             Estudando todo ecossistema de tecnologias JavaScript, React, Next.js.
