3fc2ceeb6b7b59be3f31fcbd1463666a0e0be4ba
Miguel Ângelo <miguelcg958@gmail.com>
2022-03-04 05:52:16 -0300

    chore: add footer social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,50 @@
+import { FiGithub, FiLinkedin as FiLinkedIn, FiTwitter } from 'react-icons/fi';
+
+type SocialLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+const SocialLink: React.FC<SocialLinkProps> = ({ children, ...rest }) => {
+  return (
+    <a className="p-2 transition-colors rounded-md hover:bg-gray-100" {...rest}>
+      <span className="text-xl text-gray-700 hover:text-gray-900">{children}</span>
+    </a>
+  );
+};
+
 export const Footer = () => {
   return (
     <footer className="flex justify-center w-full px-6 py-3 border-t border-gray-200 backdrop-blur bg-white/95">
       {/* Footer Wrapper */}
-      <div className="flex items-center w-full max-w-6xl justify-center md:justify-start">
+      <div className="flex flex-col items-center justify-center w-full max-w-6xl gap-2 md:flex-row md:justify-between">
         <p className="text-sm font-light text-gray-700">
           &copy; {new Date().getFullYear()} - Todos os direitos reservados.
         </p>
+
+        <div className="flex gap-2">
+          <SocialLink
+            href="https://github.com/miguel5g"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Github"
+          >
+            <FiGithub />
+          </SocialLink>
+          <SocialLink
+            href="https://www.linkedin.com/in/miguel5g/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FiLinkedIn />
+          </SocialLink>
+          <SocialLink
+            href="https://twitter.com/guel5g"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+          >
+            <FiTwitter />
+          </SocialLink>
+        </div>
       </div>
     </footer>
   );
