7b824a041fca871ca09ddab78b02040238a19ce6
Miguel Ângelo <miguelcg958@gmail.com>
2022-03-11 15:33:56 -0300

    chore: add default type to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,14 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'primary' | 'secondary' | 'tertiary';
 };
 
-export const Button: React.FC<ButtonProps> = ({ children, variant, ...rest }) => {
+export const Button: React.FC<ButtonProps> = ({ children, variant, type, ...rest }) => {
   return (
-    <button className="button" {...rest} data-variant={variant || 'primary'}>
+    <button
+      className="button"
+      type={'button' || type}
+      data-variant={variant || 'primary'}
+      {...rest}
+    >
       {children}
     </button>
   );
