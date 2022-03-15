d81298af43c8e1f586bc191b6980cdf1d4f62935
Miguel Ângelo <miguelcg958@gmail.com>
2022-03-14 21:01:36 -0300

    chore: update inputs

--- src/components/TextAreaInput.tsx
+++ src/components/TextAreaInput.tsx
@@ -1,5 +1,5 @@
 type TextAreaInputProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
-  label: string;
+  label: string | JSX.Element;
   description?: string;
   type?: undefined;
 };
@@ -7,9 +7,9 @@ type TextAreaInputProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
 export const TextAreaInput = ({ label, description, ...rest }: TextAreaInputProps) => {
   return (
     <label className="flex flex-col gap-1">
-      <span className="text-sm font-light">{label}</span>
+      <span className="flex items-center gap-1 text-sm font-light">{label}</span>
       <textarea
-        className="px-2 min-h-32 max-h-32 pt-2 pb-1.5 rounded-lg font-light bg-transparent border border-gray-200 outline-none transition-colors focus:border-cyan-900"
+        className="px-2 min-h-32 max-h-32 pt-2 pb-1.5 rounded-lg font-light bg-gray-50 focus:bg-transparent border border-gray-200 outline-none transition-colors focus:border-cyan-900"
         {...rest}
       />
       {description && <p className="text-xs font-light text-gray-700">{description}</p>}
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,15 @@
 type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  label: string;
+  label: string | JSX.Element;
   description?: string;
-  type?: undefined;
+  type?: 'text' | 'email' | 'number' | 'password';
 };
 
 export const TextInput = ({ label, description, ...rest }: TextInputProps) => {
   return (
     <label className="flex flex-col gap-1">
-      <span className="text-sm font-light">{label}</span>
+      <span className="flex items-center gap-1 text-sm font-light">{label}</span>
       <input
-        className="px-2 pt-2 pb-1.5 rounded-lg font-light bg-transparent border border-gray-200 outline-none transition-colors focus:border-cyan-900"
+        className="px-2 pt-2 pb-1.5 rounded-lg font-light bg-gray-50 focus:bg-transparent border border-gray-200 outline-none transition-colors focus:border-cyan-900"
         type="text"
         {...rest}
       />
