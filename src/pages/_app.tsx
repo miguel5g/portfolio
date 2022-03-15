7e7d0f7cf65dc6348a6908b66280caa8e97d722a
Miguel Ângelo <miguelcg958@gmail.com>
2022-03-14 21:35:59 -0300

    chore: add react-hot-toast provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster position="top-center" />
+    </>
+  );
 };
 
 export default App;
