127c6112bf1f864e7a067d229d95cd51e012682b
Miguel Ângelo <miguelcg958@gmail.com>
2022-05-14 20:14:53 -0300

    WIP: add dark colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,7 @@ class AppDocument extends Document {
           <meta property="twitter:image" content="/img/preview.png" />
         </Head>
 
-        <body className="bg-gray-50">
+        <body className="bg-gray-50 dark:bg-zinc-900">
           <Main />
           <NextScript />
         </body>
