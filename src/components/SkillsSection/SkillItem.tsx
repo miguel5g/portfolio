1c055ba67ca1494fed6f170c9aee75b48d5560ec
Miguel Ângelo <miguelcg958@gmail.com>
2022-05-14 20:54:42 -0300

    feat: add dark mode colors

--- src/components/SkillsSection/SkillItem.tsx
+++ src/components/SkillsSection/SkillItem.tsx
@@ -16,8 +16,8 @@ export const SkillItem = ({ src, alt, position, isInView }: SkillItemProps) => {
       transition={{ delay: 0.2 + position * 0.05 }}
     >
       <Image className="z-0" src={src} layout="fill" alt={alt} loader={loader} />
-      <div className="absolute inset-0 z-10 flex items-center justify-center transition opacity-0 hover:bg-white/70 hover:backdrop-blur hover:opacity-100">
-        <span className="text-lg italic font-light text-gray-900">{alt}</span>
+      <div className="absolute inset-0 z-10 flex items-center justify-center transition opacity-0 bg-white/70 dark:bg-gray-900/90 backdrop-blur hover:opacity-100">
+        <span className="text-lg italic font-light text-gray-900 dark:text-white">{alt}</span>
       </div>
     </motion.li>
   );
