abd9608080a4a6931a8407e07b68c7981868989b
Miguel Ângelo <miguelcg958@gmail.com>
2022-05-14 21:36:40 -0300

    feat: add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from 'next-themes';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
       <Toaster position="top-center" />
-    </>
+    </ThemeProvider>
   );
 };
 
