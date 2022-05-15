72f9a2d17cdda6255881a46f7002a91d95ad09b5
Miguel Ângelo <miguelcg958@gmail.com>
2022-05-14 21:35:55 -0300

    feat: add dark mode colors

--- src/components/Footer/SocialLink.tsx
+++ src/components/Footer/SocialLink.tsx
@@ -2,8 +2,11 @@ type SocialLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const SocialLink: React.FC<SocialLinkProps> = ({ children, ...rest }) => {
   return (
-    <a className="p-2 transition-colors rounded-md hover:bg-gray-100" {...rest}>
-      <span className="text-xl text-gray-700 hover:text-gray-900">{children}</span>
+    <a
+      className="p-2 transition-colors rounded-md dark:hover:bg-gray-800 hover:bg-gray-200"
+      {...rest}
+    >
+      <span className="text-xl text-gray-600 dark:text-gray-300">{children}</span>
     </a>
   );
 };
